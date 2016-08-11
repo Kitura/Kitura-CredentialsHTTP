314d998cecbbbd6224351c48d7718a221676d808
Ian Partridge <i.partridge@uk.ibm.com>
2016-08-11 12:17:03 +0100

    updated dependency versions in Package.swift
    set version of https://github.com/IBM-Swift/Kitura-Credentials.git to 0.26.0

--- Package.swift
+++ Package.swift
@@ -19,6 +19,6 @@ import PackageDescription
 let package = Package(
     name: "Kitura-CredentialsHTTP",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", majorVersion: 0, minor: 25),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", majorVersion: 0, minor: 26),
     ]
 )
