f4dcf2bce02b7e87822f8cadf3be8298fbebab15
Carl Brown <carlb@pdagent.com>
2017-03-21 18:23:20 -0500

    fix swift-31 regex (#38)

--- Sources/CredentialsHTTP/CredentialsHTTPDigest.swift
+++ Sources/CredentialsHTTP/CredentialsHTTPDigest.swift
@@ -163,7 +163,11 @@ public class CredentialsHTTPDigest : CredentialsPluginProtocol {
     }
     
     #if os(Linux)
-        typealias RegularExpressionType = RegularExpression
+        #if swift(>=3.1)
+            typealias RegularExpressionType = NSRegularExpression
+        #else
+            typealias RegularExpressionType = RegularExpression
+        #endif
     #else
         typealias RegularExpressionType = NSRegularExpression
     #endif
