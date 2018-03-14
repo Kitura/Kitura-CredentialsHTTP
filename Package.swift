953c618037b297548325a293097446cf193beb22
Andrew Lees <32634907+Andrew-Lees11@users.noreply.github.com>
2018-03-14 16:12:49 +0000

    Update Kitura-Credentials dependency to "from: "2.0.0"" (#47)

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
         )
     ],    
     dependencies: [
-         .package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", .upToNextMinor(from: "2.0.0")),
+         .package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", from: "2.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
